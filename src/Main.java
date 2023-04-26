9716cb7be9d52e6809507875640115c8cd1c201a
Artem <s-art7@bk.ru>
2023-04-26 15:58:39 +0300

    InitialCommit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 9456;
+        int bonusAmount = 20;
+        int miles = ticket / bonusAmount;
+        System.out.println(miles);
+    }
+}
